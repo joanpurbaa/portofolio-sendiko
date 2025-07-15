629910c4914be65deec11c089dd0189e4c23567d
RaditAtalla <radityaderachmadie@gmail.com>
2025-07-15 12:00:42 +0700

    Feat: add my experience section

--- components/landing/experience-section.tsx
+++ components/landing/experience-section.tsx
@@ -1,10 +1,69 @@
+const EXPERIENCES = [
+  {
+    title: "Mobile Developer",
+    company: "Direktorat Pusat Informasi, Telkom University",
+    duration: "July 2025 - Now",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adispicing elit. Lorem ipsum dolor sit amet consectetur adispicing elit.Lorem ipsum dolor sit amet consectetur adispicing elit.",
+  },
+  {
+    title: "Android Development Mentor",
+    company: "Chevalier Laboratory SAS, School of Applied Science",
+    duration: "August 2024 - Now",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adispicing elit. Lorem ipsum dolor sit amet consectetur adispicing elit.Lorem ipsum dolor sit amet consectetur adispicing elit.",
+  },
+  {
+    title: "Mobile Developer",
+    company: "Freelance",
+    duration: "July 2023 - Now",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adispicing elit. Lorem ipsum dolor sit amet consectetur adispicing elit.Lorem ipsum dolor sit amet consectetur adispicing elit.",
+  },
+  {
+    title: "Kotlin Mentor",
+    company: "Dilesin Academy",
+    duration: "October 2024 - November 2024",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adispicing elit. Lorem ipsum dolor sit amet consectetur adispicing elit.Lorem ipsum dolor sit amet consectetur adispicing elit.",
+  },
+  {
+    title: "Android Developer",
+    company: "PT. Puskomedia Indonesia Kreatif",
+    duration: "April 2022 - September 2022",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adispicing elit. Lorem ipsum dolor sit amet consectetur adispicing elit.Lorem ipsum dolor sit amet consectetur adispicing elit.",
+  },
+];
+
 export default function ExperienceSection() {
   return (
-    <section
-      id="experience-section"
-      className="min-h-screen flex flex-col items-center"
-    >
+    <section id="experience-section" className="min-h-screen">
       <h2 className="font-light text-4xl mb-12">My Experience</h2>
+
+      <div className="flex flex-col">
+        {EXPERIENCES.map((experience, index) => (
+          <div
+            key={index}
+            className="relative border-l-2 border-primary pl-6 pb-2"
+          >
+            <div className="size-4 rounded-full bg-primary absolute z-10 -left-2.5 -top-2.5"></div>
+
+            <div>
+              <div>
+                <div className="flex items-baseline gap-2">
+                  <h3 className="font-medium text-2xl">{experience.title}</h3>
+                  <p className="font-extrabold text-sm text-primary">
+                    {experience.duration}
+                  </p>
+                </div>
+                <p className="font-bold">at {experience.company}</p>
+              </div>
+              <p className="mt-4 font-medium">{experience.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
