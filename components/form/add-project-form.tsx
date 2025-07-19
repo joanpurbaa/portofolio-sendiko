d69eba2a3ab8859711179cb91929d76da1ccf5b1
RaditAtalla <radityaderachmadie@gmail.com>
2025-07-19 21:37:20 +0700

    Feat: add placeholder to AddProjectForm inputs

--- components/form/add-project-form.tsx
+++ components/form/add-project-form.tsx
@@ -5,9 +5,27 @@ export default function AddProjectForm() {
     <form className="mt-8 p-10 bg-neutral-800 rounded-lg">
       <h2 className="text-xl font-bold mb-8">Tambah projek Baru</h2>
 
-      <TextInput id="judul" label="Judul" required className="mb-2" />
-      <TextInput id="deskripsi" label="Deskripsi" required className="mb-2" />
-      <TextInput id="tech" label="Tech Stack" required className="mb-2" />
+      <TextInput
+        id="judul"
+        label="Judul"
+        required
+        className="mb-2"
+        placeholder="Projek terbaru saya"
+      />
+      <TextInput
+        id="deskripsi"
+        label="Deskripsi"
+        required
+        className="mb-2"
+        placeholder="Lorem ipsum dolor sit amet."
+      />
+      <TextInput
+        id="tech"
+        label="Tech Stack"
+        required
+        className="mb-2"
+        placeholder="kotlin, laravel"
+      />
 
       <fieldset className="inline-flex flex-col gap-2">
         <label htmlFor="image-preview" className="font-medium">
@@ -15,7 +33,7 @@ export default function AddProjectForm() {
         </label>
         <input
           type="file"
-          className="border-2 border-neutral-700 rounded-lg p-3"
+          className="border-2 border-neutral-700 rounded-lg p-3 text-sm"
         />
       </fieldset>
 
